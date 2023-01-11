9307d3537aa93118a690c58506cf369c79fd851c
benji <Benji59btw@gmail.com>
2023-01-11 13:48:13 +0100

    fini

--- index.php
+++ index.php
@@ -2,19 +2,28 @@
 
 // Premiere ligne
 
-{
+function helloWorld(){
     echo "Hello World!";
 }
 
 //Deuxieme ligne
-
+helloWorld();
 
 
 //Troisieme ligne
 
 function maFonctionParam($fname, $lname) {
-    echo ;
+    echo "hello $fname $lname";
+    if (is_float($fname)){
+        return intval($fname );
+    }
+    if (is_float($lname)){
+        return intval($lname );
+    }
+    return false;
 }
+
+maFonctionParam('bla','blou');
 
 
 
